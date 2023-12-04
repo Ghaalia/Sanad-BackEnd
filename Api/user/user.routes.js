dc19ec9e50731c9be1adaeb02a1607956e545392
lianne <liannealq@hotmail.com>
2023-12-04 20:21:43 +0300

    ...

--- Api/user/user.routes.js
+++ Api/user/user.routes.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const User = require("../../models/User");
-const { register, signin, getAllUsers } = require("./user.controllers");
+const {
+  register,
+  signin,
+  getAllUsers,
+
+  deleteUser,
+  updateUser,
+} = require("./user.controllers");
 const passport = require("passport");
 const upload = require("../../middleware/multer");
 
 const userrouter = express.Router();
 
-userrouter.post("/register", upload.single("image"), register);
+userrouter.post("/user/register", upload.single("image"), register);
 
 userrouter.post(
-  "/signin",
+  "/user/login",
   passport.authenticate("local", { session: false }),
   signin
 );
 
-userrouter.get("/allusers", getAllUsers);
+userrouter.get("/user/allusers", getAllUsers);
+
+userrouter.put(
+  "/user/updateuser",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("image"),
+  updateUser
+);
+
+userrouter.delete(
+  "/user/deleteuser",
+  passport.authenticate("jwt", { session: false }),
+  deleteUser
+);
 
 module.exports = userrouter;
