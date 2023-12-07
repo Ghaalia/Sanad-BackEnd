6166fa9e5f1ea972f9e087ca32facda8efe74d2b
lianne <liannealq@hotmail.com>
2023-12-07 21:19:11 +0300

    commit notification

--- Api/device/device.routes.js
+++ Api/device/device.routes.js
@@ -1,17 +1,17 @@
-// routes/notification.routes.js
-const express = require("express");
-const router = express.Router();
+// // routes/notification.routes.js
+// const express = require("express");
+// const router = express.Router();
 
-const {
-  createNotification,
-  getNotificationsByUser,
-} = require("../notification/notification.controllers");
-const { registerDevice } = require("./device.controllers");
+// const {
+//   createNotification,
+//   getNotificationsByUser,
+// } = require("../notification/notification.controllers");
+// const { registerDevice } = require("./device.controllers");
 
-router.post("/notifications", createNotification);
-router.get("/notifications/:userId", getNotificationsByUser);
+// router.post("/notifications", createNotification);
+// router.get("/notifications/:userId", getNotificationsByUser);
 
-// New route for registering device tokens
-router.post("/register_token", registerDevice);
+// // New route for registering device tokens
+// router.post("/register_token", registerDevice);
 
-module.exports = router;
+// module.exports = router;
